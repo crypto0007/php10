35932e595969864ac9c16827a9449f0e1427e813
crypto0007 <50778638+crypto0007@users.noreply.github.com>
2019-09-26 12:34:55 +0530

    Add files via upload

---
+++ suggest.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>Suggestions</title>
+</head>
+<body>
+	<form action="" method="POST">
+		<table align="center">
+			<tr>
+				<td>
+					<label for="sug">Suggestions</label>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<input type="text" name="sugg">
+				</td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
